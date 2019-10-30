9f9c39db33881f7c3c1f72a60bc0b8d8c1207b6d
tupyl2 <lyssamt@gmail.com>
2019-10-29 20:35:54 -0400

    Test #2

--- Project2/src/main/java/com/project2/model/Post.java
+++ Project2/src/main/java/com/project2/model/Post.java
@@ -1,8 +1,11 @@
 package com.project2.model;
 
+import java.util.List;
+
 public class Post {
 	
 	private User creator; 
 	private String text;
-
+	private List<String> images; 
+//	private Timestamp time; 
 }
