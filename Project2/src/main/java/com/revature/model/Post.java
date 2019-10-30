f08eb46f9a462344568b7df5ee3787121bc26eea
Davin-Merry <dmerry@garcat.com>
2019-10-30 10:07:18 -0400

    Defined that 'User has a Profile' object. Minor formatting corrections.

--- Project2/src/main/java/com/revature/model/Post.java
+++ Project2/src/main/java/com/revature/model/Post.java
@@ -12,40 +12,52 @@ public class Post {
 	private List<String> images; 
 	private Timestamp time; 
 	private Set<User> likes;
+	
 	public User getCreator() {
 		return creator;
 	}
+	
 	public void setCreator(User creator) {
 		this.creator = creator;
 	}
+	
 	public String getText() {
 		return text;
 	}
+	
 	public void setText(String text) {
 		this.text = text;
 	}
+	
 	public List<String> getImages() {
 		return images;
 	}
+	
 	public void setImages(List<String> images) {
 		this.images = images;
 	}
+	
 	public Timestamp getTime() {
 		return time;
 	}
+	
 	public void setTime(Timestamp time) {
 		this.time = time;
 	}
+	
 	public Set<User> getLikes() {
 		return likes;
 	}
+	
 	public void setLikes(Set<User> likes) {
 		this.likes = likes;
 	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(creator, images, likes, text, time);
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,6 +71,7 @@ public class Post {
 				&& Objects.equals(likes, other.likes) && Objects.equals(text, other.text)
 				&& Objects.equals(time, other.time);
 	}
+	
 	public Post(User creator, String text, List<String> images, Timestamp time, Set<User> likes) {
 		super();
 		this.creator = creator;
@@ -67,16 +80,15 @@ public class Post {
 		this.time = time;
 		this.likes = likes;
 	}
+	
 	public Post() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
 	@Override
 	public String toString() {
 		return "Post [creator=" + creator + ", text=" + text + ", images=" + images + ", time=" + time + ", likes="
 				+ likes + "]";
-	} 
-	
-	
-	
+	}
 }
--- Project2/src/main/java/com/revature/model/User.java
+++ Project2/src/main/java/com/revature/model/User.java
@@ -1,42 +1,65 @@
 package com.revature.model;
 
-import java.util.Objects;
-
 public class User {
 	
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
+	private Profile profile;
 	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public Profile getProfile() {
+		return this.profile;
+	}
+	
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(email, firstName, lastName, password);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((profile == null) ? 0 : profile.hashCode());
+		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -46,9 +69,34 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password);
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (profile == null) {
+			if (other.profile != null)
+				return false;
+		} else if (!profile.equals(other.profile))
+			return false;
+		return true;
 	}
+	
 	public User(String firstName, String lastName, String email, String password) {
 		super();
 		this.firstName = firstName;
@@ -56,16 +104,15 @@ public class User {
 		this.email = email;
 		this.password = password;
 	}
+	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
 	@Override
 	public String toString() {
 		return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password=" + password
-				+ "]";
+				+ ", profile=" + profile + "]";
 	}
-	
-	
-
 }
